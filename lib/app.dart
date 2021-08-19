d26a4e94f17f4cef789216682e7b53c865d032ff
Sangam Kumar Shrestha <2shrestha22@gmail.com>
2021-08-19 12:12:16 +0545

    theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_template/routes/routes.dart';
+import 'package:flutter_template/theme/theme.dart';
 
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
       routerDelegate: AutoRouterDelegate(appRouter),
       routeInformationParser: appRouter.defaultRouteParser(),
       title: 'Flutter Template',
+      theme: defaultTheme,
     );
   }
 }
